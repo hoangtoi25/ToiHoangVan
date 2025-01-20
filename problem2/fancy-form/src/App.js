6b5f5adb52763ab9289dc0aa58f6b6a14b2a03f2
LGE\toi.hoang <toi.hoang@lge.com>
2025-01-20 15:57:17 +0700

    resolve problem2

---
+++ problem2/fancy-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CurrencySwapForm from './CurrencySwapForm';  
+
+const App = () => {  
+  return (  
+      <div>  
+          <CurrencySwapForm />  
+      </div>  
+  );  
+};  
+
+export default App;  
